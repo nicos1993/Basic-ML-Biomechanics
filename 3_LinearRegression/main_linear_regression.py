3bcda887a6bbb3b6732c84123439cee558ce20f6
Nicos <nicosharalabidis@stanford.edu>
2024-11-07 14:03:29 -0800

    linear regression

---
+++ 3_LinearRegression/main_linear_regression.py
@@ -0,0 +1,8 @@
+# Linear regression
+# DataSet: https://zenodo.org/records/13788592
+# Article: Ruth, PS., Uhlrich, S., de Monts, C., Falisse, A., Muccini, J.,
+# Covitz, S., Vogt-Domke, S., Karman, L., Ismail., S., Ataide, P., Ong, C.,
+# Day, J., Duong, T., Delp, S. 2024
+# Kinematics and timed function tests of facioscapulohumeral muscular dystrophy
+# and myotonic dystrophy
+
