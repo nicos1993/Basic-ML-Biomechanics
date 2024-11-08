8049bb011105144cf7dd58e9125b385ab10414a7
Nicos <nicosharalabidis@stanford.edu>
2024-11-07 19:31:06 -0800

    prepping data

--- 3_LinearRegression/main_linear_regression.py
+++ 3_LinearRegression/main_linear_regression.py
@@ -9,6 +9,7 @@
 import os
 import sys
 import pandas as pd
+import numpy as np
 
 # Set the folder path to where the data is located; change accordingly
 dataFolder = "C:\\Users\\Nicos\\Documents\\FHSD_Data"
@@ -22,15 +23,24 @@ data = pd.read_csv(os.path.join(dataFolder,dataFile))
 # Remove duplicate instances of participants in data based on subject ID, keep first occurence
 data.drop_duplicates(subset=['ID'],keep='first',inplace=True)
 
-# Data features can be found from:
-# data.columns
-
-# The number of participants (rows) and ~features~ (column) can be found from:
-# data.shape
-
 # Check if the data folder exists, if not return error message
 if not os.path.isdir(dataFolder):
     print(f"The current data folder directory {dataFolder} does not exist.")
     print(f"Set the 'dataFolder' variable to the correct folder.")
     sys.exit()
+
+# Data features can be found from:
+# data.columns
+
+# The number of participants (rows) and ~features~ (columns) can be found from:
+# data.shape
+
+# Reinitialize a data frame with the necessary features (predictors and outcome)
+features = ['10mwrt_speed','10mwrt_com_sway','tug_cone_turn_avel','tug_cone_time']
+data = data[features]
+
+# Convert from data frame object to numpy array
+data_np = data.to_numpy()
+
+print('break point')
 
