50365fa98c3e3691ef41eabb32ab709f66797a9d
Nicos <nicosharalabidis@stanford.edu>
2024-11-14 17:03:54 -0800

    small test

--- 3_LinearRegression/main_linear_regression.py
+++ 3_LinearRegression/main_linear_regression.py
@@ -11,11 +11,20 @@ import sys
 import pandas as pd
 import numpy as np
 import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from utilityFunctions import *
 
 # Set the folder path to where the data is located; change accordingly
 dataFolder = "C:\\Users\\Nicos\\Documents\\FHSD_Data"
+
+# Check if the data folder exists, if not return error message
+if not os.path.isdir(dataFolder):
+    print(f"The current data folder directory {dataFolder} does not exist.")
+    print(f"Set the 'dataFolder' variable to the correct folder.")
+    sys.exit()
 
 # Data file
 dataFile = "video_features.csv"
@@ -25,12 +34,6 @@ data = pd.read_csv(os.path.join(dataFolder,dataFile))
 
 # Remove duplicate instances of participants in data based on subject ID, keep first occurence
 data.drop_duplicates(subset=['ID'],keep='first',inplace=True)
-
-# Check if the data folder exists, if not return error message
-if not os.path.isdir(dataFolder):
-    print(f"The current data folder directory {dataFolder} does not exist.")
-    print(f"Set the 'dataFolder' variable to the correct folder.")
-    sys.exit()
 
 # Data features can be found from:
 # data.columns
@@ -54,14 +57,34 @@ data_y = data_np[:,-1]
 
 X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, test_size=0.15, random_state=42)
 
-scaler_train_X = StandardScaler()
-scaler_train_y = StandardScaler()
+X_mean = X_train.mean(axis=0)
+X_std  = X_train.std(axis=0)
+X_train_norm = (X_train - X_mean) / X_std
 
-scaler_train_X.fit(X_train)
-scaler_train_y.fit(y_train.reshape(-1,1))
+y_mean = y_train.mean(axis=0)
+y_std  = y_train.std(axis=0)
+y_train_norm = (y_train - y_mean) / y_std
 
-norm_X_train = scaler_train_X.transform(X_train)
-norm_y_train = scaler_train_y.transform(y_train.reshape(-1,1))
+X_test_norm = (X_test - X_mean) / X_std
+y_test_norm = (y_test - y_mean) / y_std
+
+training_data = CustomDataset(labels_file=y_train_norm, features_file=X_train_norm)
+
+training_dataloader = DataLoader(training_data, batch_size=8, shuffle=True)
+
+# layers = []
+#
+# layers.append(nn.Linear(4,2))
+# layers.append(nn.ReLU())
+#
+# print(layers)
+#
+# print(*layers)
+# model = nn.Sequential()
+
+for inputs, labels in training_dataloader:
+    print(labels)
+
 
 print('break point')
 
